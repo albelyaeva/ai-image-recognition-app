c2cae0c839439592fed8d5bab599f900ef55b791
aleksandra <al.belyaeva1@gmail.com>
2025-01-29 18:43:00 +0100

    Fix OpenCV missing libGL on Railway

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Install missing OpenCV dependencies
-apt-get update && apt-get install -y libgl1-mesa-glx
+apt-get update && apt-get install -y libgl1-mesa-glx libglib2.0-0
 # Start Flask using Gunicorn
 gunicorn -w 4 -b 0.0.0.0:$PORT app:app
